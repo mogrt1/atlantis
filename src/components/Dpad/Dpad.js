f63d90e6b91cff845a5f63ea99f58167ef9a7af6
Brian Blakely <brian.blakely@aexp.com>
2018-06-07 18:14:43 -0400

    Basic pointer event and keyboard event flows for game controls.

--- src/components/Dpad/Dpad.js
+++ src/components/Dpad/Dpad.js
@@ -1,10 +1,39 @@
 import React from 'react';
+
 import DpadView from './DpadView';
+import './Dpad.css';
+import KeyCommands from '../KeyCommands';
 
 export default class Dpad extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const stop = (e)=> {
+      e.stopPropagation();
+      e.preventDefault();
+
+      return true;
+    };
+
+    this.pointerEvents = {
+      down: (e)=> stop(e) && console.log(`down`, e),
+      move: (e)=> stop(e) && console.log(`move`, e),
+      up: (e)=> stop(e) && console.log(`up`, e)
+    };
+
+    this.keyEvents = {
+      ';': this.pointerEvents
+    };
+  }
+
   render() {
     return (
-      <DpadView></DpadView>
+      <>
+        <DpadView {...this.pointerEvents} />
+        <KeyCommands>
+          {this.keyEvents}
+        </KeyCommands>
+      </>
     );
   }
 }
