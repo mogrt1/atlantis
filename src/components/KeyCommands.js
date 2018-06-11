729d1e59ebf2583d4637ccc3b3863cef77f52bb4
Brian Blakely <brian.blakely@aexp.com>
2018-06-11 16:59:04 -0400

    Unbind key events when unmounting.

--- src/components/KeyCommands.js
+++ src/components/KeyCommands.js
@@ -2,14 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class KeyCommands extends React.Component {
-  componentDidMount() {
-    document.addEventListener(`keydown`, (e)=> {
-      if(!(e.key in this.props.children)) {
+  constructor(props) {
+    super(props);
+
+    this.eventDelegate = (e)=> {
+      if(!(e.key in props.children)) {
         return false;
       }
 
-      this.props.children[e.key].down(e);
-    });
+      props.children[e.key].down(e);
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener(`keydown`, this.eventDelegate);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener(`keydown`, this.eventDelegate);
   }
 
   render() {
