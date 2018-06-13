92686788489c6a7d23eef4f2f61e24c981b26fd7
Brian Blakely <brian.blakely@aexp.com>
2018-06-13 14:02:09 -0400

    Component that plays free homebrew demo game.

---
+++ src/components/Demo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Loader from './Loader';
+import { Consumer } from './Store';
+
+import demo from '../demo.gb';
+
+export default class Demo extends React.Component {
+  render() {
+    return (
+      <Consumer>
+        {({ state })=> (
+          <React.Fragment>
+            {
+              state.canvas && state.canvas.current
+                && <Loader uri={demo} canvas={state.canvas.current} />
+            }
+          </React.Fragment>
+        )}
+      </Consumer>
+    );
+  }
+}
