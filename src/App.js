217d366b4dc05e22e245dff8e698c49a87c35ca4
Brian Blakely <brian.blakely@aexp.com>
2018-06-13 14:01:22 -0400

    Setup app component to actually contain an app.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import './App.css';
-import Store from './components/Store';
-import Dpad from './components/Dpad/Dpad';
+import Store, { Consumer } from './components/Store';
+import Demo from './components/Demo';
+import Gamepad from './components/Gamepad/GamepadView';
+import Emulator from './components/Emulator/Emulator';
 
 class App extends React.Component {
   render() {
     return (
       <Store>
-        <Dpad></Dpad>
+        <React.Fragment>
+          <Demo />
+          <Gamepad />
+          <Consumer>
+            {(context)=> <Emulator setCanvas={context.actions.setCanvas} />}
+          </Consumer>
+        </React.Fragment>
       </Store>
     );
   }
