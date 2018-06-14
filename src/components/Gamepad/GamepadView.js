951d00729477408df2406cdda069fb5627288d14
Brian Blakely <brian.blakely@aexp.com>
2018-06-14 15:27:54 -0400

    Add essential buttons to gampad.

--- src/components/Gamepad/GamepadView.js
+++ src/components/Gamepad/GamepadView.js
@@ -4,12 +4,25 @@ import { pure } from 'recompose';
 
 import { Consumer } from '../Store';
 import Dpad from '../Dpad/Dpad';
+import GamepadButton from '../GamepadButton/GamepadButton';
 
 const GamepadView = ()=> (
   <Consumer>
     {()=> (
       <div className="Gamepad">
         <Dpad />
+        <GamepadButton type="B" key="z">
+          {`B`}
+        </GamepadButton>
+        <GamepadButton type="A" key="x">
+          {`A`}
+        </GamepadButton>
+        <GamepadButton type="START" key="Enter">
+          {`Start`}
+        </GamepadButton>
+        <GamepadButton type="SELECT" key="Shift">
+          {`Select`}
+        </GamepadButton>
       </div>
     )}
   </Consumer>
