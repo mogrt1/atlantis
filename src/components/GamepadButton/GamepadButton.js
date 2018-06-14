0e470bf713cd1662f94421024611d765d9d4c5c8
Brian Blakely <brian.blakely@aexp.com>
2018-06-14 15:27:35 -0400

    Container component for gamepad button input.

---
+++ src/components/GamepadButton/GamepadButton.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Button from '../Button/Button';
+
+import { GameBoyJoyPadEvent as gameBoyJoyPadEvent } from '../../cores/GameBoy-Online/js/index';
+
+export default class GamepadButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const buttonCodes = {
+      START: 7,
+      SELECT: 6,
+      A: 4,
+      B: 5
+    };
+
+    this.events = {
+      down: ()=> {
+        gameBoyJoyPadEvent(buttonCodes[props.type], `pressed`);
+      },
+      up: ()=> {
+        gameBoyJoyPadEvent(buttonCodes[props.type]);
+      }
+    };
+
+    this.keyEvents = { [props.key]: this.events };
+  }
+
+  render() {
+    return (
+      <Button
+        className={this.props.className}
+        pointerCommands={this.events}
+        keyCommands={this.keyEvents}
+      >
+        {this.props.children}
+      </Button>
+    );
+  }
+}
+
+GamepadButton.propTypes = {
+  type: PropTypes.string.isRequired,
+  key: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node
+};
