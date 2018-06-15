94e497c527cf4ab1fea952351dbb4423832caa24
Brian Blakely <brian.blakely@aexp.com>
2018-06-15 14:02:10 -0400

    Prevent default scrolling and selection behaviors within the app.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById(`root`));
+const root = document.getElementById(`root`);
+
+root.addEventListener(`touchmove`, (e)=> e.preventDefault());
+root.addEventListener(`mousemove`, (e)=> e.preventDefault());
+
+ReactDOM.render(<App />, root);
 registerServiceWorker();
