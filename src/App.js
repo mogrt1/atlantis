d3374e05644d88e21985d6144298c93e907d3d09
Brian Blakely <brian.blakely@aexp.com>
2018-06-15 13:55:19 -0400

    Disable demo and add settings component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Store, { Consumer } from './components/Store';
-import Demo from './components/Demo';
+// import Demo from './components/Demo';
 import Gamepad from './components/Gamepad/GamepadView';
 import Emulator from './components/Emulator/Emulator';
+import Settings from './components/Settings/Settings';
 
 class App extends React.Component {
   render() {
     return (
       <Store>
         <React.Fragment>
-          <Demo />
+          {/* <Demo /> */}
+          <Settings />
           <Gamepad />
           <Consumer>
             {(context)=> <Emulator setCanvas={context.actions.setCanvas} />}
