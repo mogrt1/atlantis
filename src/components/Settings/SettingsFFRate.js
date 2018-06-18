efdb29573e484f56a47ed2785aa8ff612130d216
Brian Blakely <brian.blakely@aexp.com>
2018-06-18 16:55:18 -0400

    Make FF rate more DRY; Add setting for toggling toggle/hold behavior.

--- src/components/Settings/SettingsFFRate.js
+++ src/components/Settings/SettingsFFRate.js
@@ -4,21 +4,33 @@ import PropTypes from 'prop-types';
 import { styleSettingsFFRate } from './SettingsStyles';
 
 import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Switch from '@material-ui/core/Switch';
 import FastForwardIcon from '@material-ui/icons/FastForward';
 
 class SettingsFFRate extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { rate: 2 };
+    this.state = {
+      rate: 2,
+      toggle: true
+    };
 
-    this.handleChange = (e)=> {
-      this.setState({ [e.target.name]: e.target.value });
+    this.rateLevels = 9;
+    this.startRate = 2;
+
+    this.changeRate = (e)=> {
+      this.setState({ rate: e.target.value });
+    };
+
+    this.toggleToggle = ()=> {
+      this.setState({ toggle: !this.state.toggle });
     };
   }
 
@@ -26,32 +38,46 @@ class SettingsFFRate extends React.Component {
     const { classes } = this.props;
 
     return (
-      <ListItem>
-        <ListItemIcon>
-          <FastForwardIcon />
-        </ListItemIcon>
-        <FormControl className={classes.select}>
-          <InputLabel htmlFor="settings-ff-rate">Fast-Forward Rate</InputLabel>
-          <Select
-            value={this.state.rate}
-            onChange={this.handleChange}
-            inputProps={{
-              name: `settings-ff-rate`,
-              id: `settings-ff-rate`
-            }}
-          >
-            <MenuItem value={2}>2x</MenuItem>
-            <MenuItem value={3}>3x</MenuItem>
-            <MenuItem value={4}>4x</MenuItem>
-            <MenuItem value={5}>5x</MenuItem>
-            <MenuItem value={6}>6x</MenuItem>
-            <MenuItem value={7}>7x</MenuItem>
-            <MenuItem value={8}>8x</MenuItem>
-            <MenuItem value={9}>9x</MenuItem>
-            <MenuItem value={10}>10x</MenuItem>
-          </Select>
-        </FormControl>
-      </ListItem>
+      <React.Fragment>
+        <ListItem>
+          <ListItemIcon>
+            <FastForwardIcon />
+          </ListItemIcon>
+          <FormControl className={classes.select}>
+            <InputLabel htmlFor="settings-ff-rate">Fast-Forward Rate</InputLabel>
+            <Select
+              value={this.state.rate}
+              onChange={this.changeRate}
+              inputProps={{
+                name: `settings-ff-rate`,
+                id: `settings-ff-rate`
+              }}
+            >
+              {Array(this.rateLevels).fill(`0`).map((el, index)=> (
+                <MenuItem
+                  key={el + index}
+                  value={index + this.startRate}
+                >
+                  {`${index + this.startRate}x`}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </ListItem>
+
+        <ListItem button onClick={this.toggleToggle}>
+          <ListItemIcon>
+            <FastForwardIcon />
+          </ListItemIcon>
+          <ListItemText className={classes.itemText}>
+            {`FF Toggle`}
+          </ListItemText>
+          <Switch checked={this.state.toggle} classes={{
+            root: classes.toggleSwitch,
+            checked: classes.toggleSwitchChecked
+          }} />
+        </ListItem>
+      </React.Fragment>
     );
   }
 }
