03b6113f2fe32aa6b4e97eee4602b4332aa27578
Brian Blakely <brian.blakely@aexp.com>
2018-06-18 13:46:02 -0400

    Make SettingsKeyBindings more DRY.

--- src/components/Settings/SettingsKeyBindings.js
+++ src/components/Settings/SettingsKeyBindings.js
@@ -17,20 +17,58 @@ class SettingsKeyBindings extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      open: false,
-      'settings-kb-b': `z`,
-      'settings-kb-a': `x`,
-      'settings-kb-b-turbo': `a`,
-      'settings-kb-a-turbo': `s`,
-      'settings-kb-start': `Enter`,
-      'settings-kb-select': `Shift`,
-      'settings-kb-up': `ArrowUp`,
-      'settings-kb-down': `ArrowDown`,
-      'settings-kb-left': `ArrowLeft`,
-      'settings-kb-right': `ArrowRight`,
-      'settings-kb-ff': `\``
+    this.state = { open: false };
+
+    this.bindings = {
+      'b': {
+        default: `z`,
+        label: `B`
+      },
+      'a': {
+        default: `x`,
+        label: `A`
+      },
+      'b-turbo': {
+        default: `a`,
+        label: `B Turbo`
+      },
+      'a-turbo': {
+        default: `s`,
+        label: `A Turbo`
+      },
+      'start': {
+        default: `Enter`,
+        label: `Start`
+      },
+      'select': {
+        default: `Shift`,
+        label: `Select`
+      },
+      'up': {
+        default: `ArrowUp`,
+        label: `Up`
+      },
+      'down': {
+        default: `ArrowDown`,
+        label: `Down`
+      },
+      'left': {
+        default: `ArrowLeft`,
+        label: `Left`
+      },
+      'right': {
+        default: `ArrowRight`,
+        label: `Right`
+      },
+      'ff': {
+        default: `\``,
+        label: `Fast-Forward`
+      }
     };
+
+    for(const [id, data] of Object.entries(this.bindings)) {
+      this.state[`settings-kb-${id}`] = data.default;
+    }
 
     this.toggleBindings = ()=> {
       this.setState({ open: !this.state.open });
@@ -57,105 +95,17 @@ class SettingsKeyBindings extends React.Component {
         </ListItem>
         <Collapse in={this.state.open} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-b"
-                label="B"
-                value={this.state[`settings-kb-b`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-a"
-                label="A"
-                value={this.state[`settings-kb-a`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-b-turbo"
-                label="B Turbo"
-                value={this.state[`settings-kb-b-turbo`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-a-turbo"
-                label="A Turbo"
-                value={this.state[`settings-kb-a-turbo`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-start"
-                label="Start"
-                value={this.state[`settings-kb-start`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-select"
-                label="Select"
-                value={this.state[`settings-kb-select`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-up"
-                label="Up"
-                value={this.state[`settings-kb-up`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-down"
-                label="Down"
-                value={this.state[`settings-kb-down`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-left"
-                label="Left"
-                value={this.state[`settings-kb-left`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-right"
-                label="Right"
-                value={this.state[`settings-kb-right`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
-            <ListItem className={classes.nested} dense divider>
-              <TextField
-                id="settings-kb-ff"
-                label="Fast-Forward"
-                value={this.state[`settings-kb-ff`]}
-                onKeyDown={this.assignKey}
-                margin="normal"
-              />
-            </ListItem>
+            {Object.entries(this.bindings).map(([id, data])=> (
+              <ListItem key={id} className={classes.nested} dense divider>
+                <TextField
+                  id={`settings-kb-${id}`}
+                  label={data.label}
+                  value={this.state[`settings-kb-${id}`]}
+                  onKeyDown={this.assignKey}
+                  margin="normal"
+                />
+              </ListItem>
+            ))}
           </List>
         </Collapse>
       </React.Fragment>
