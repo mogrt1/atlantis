af96238510de1b5df22830a4291a2b24e2d6e2fa
Brian Blakely <brian.blakely@aexp.com>
2018-06-18 16:26:16 -0400

    Make some elements amber.

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,11 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import grey from '@material-ui/core/colors/grey';
+import amber from '@material-ui/core/colors/amber';
 
 const theme = createMuiTheme({
   palette: {
     primary: grey,
+    secondary: amber,
     type: `dark`
   }
 });
