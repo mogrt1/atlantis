6c39c83a24943b563bbe58799a9df2ed2f755588
Brian Blakely <brian.blakely@aexp.com>
2018-06-18 12:14:24 -0400

    Add file to library from file upload dialog.

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,17 +1,61 @@
 import React, { createContext } from 'react';
 import PropTypes from 'prop-types';
 
+import Spark from 'spark-md5';
+
+import { games } from '../db/gameboy.js';
+
 const { Provider, Consumer } = createContext();
 
 export default class Store extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = { library: [] };
 
     this.actions = {
       setCanvas: (canvas)=> {
         this.setState({ canvas });
+      },
+
+      addGame: (e)=> {
+        const getMd5 = (file)=> new Promise((resolve, reject)=> {
+          const reader = new FileReader();
+
+          reader.onload = (re)=> {
+            const buffer = new Spark.ArrayBuffer();
+
+            buffer.append(re.target.result);
+            resolve({
+              fileName: file.name,
+              hash: buffer.end().toUpperCase()
+            });
+          };
+
+          reader.onerror = (err)=> {
+            reject(err);
+          };
+
+          reader.readAsArrayBuffer(file);
+        });
+
+        const md5s = [];
+
+        for(const file of e.target.files) {
+          md5s.push(getMd5(file));
+        }
+
+        Promise.all(md5s).then((result)=> {
+          this.setState({
+            library: [
+              ...this.state.library,
+              ...result.map((md5)=> ({
+                md5: md5.hash,
+                title: games[md5.hash] || md5.fileName
+              }))
+            ]
+          });
+        });
       }
     };
   }
