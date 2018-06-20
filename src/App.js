48a78d59d78daf809c072cfa9e90e908619a8c8c
Brian Blakely <anewpage.media@gmail.com>
2018-06-20 03:16:11 -0400

    Restore data from local database.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { get, keys } from 'idb-keyval';
+
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './theme';
@@ -11,7 +13,31 @@ import Emulator from './components/Emulator/Emulator';
 import Settings from './components/Settings/Settings';
 import Library from './components/Library/Library';
 
+import { persistValues } from './cores/GameBoy-Online/js/index';
+
+const restoreData = async function() {
+  const data = await keys();
+
+  const valuesTx = [];
+
+  for(const datum of data) {
+    valuesTx.push(get(datum));
+  }
+
+  const values = await Promise.all(valuesTx);
+
+  for(const [index, value] of values.entries()) {
+    persistValues[data[index]] = value;
+  }
+};
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    restoreData();
+  }
+
   render() {
     return (
       <Context>
