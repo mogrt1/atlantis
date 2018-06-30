cd26d9664a21f64aad702c68bc56fc72941928a4
Brian Blakely <anewpage.media@gmail.com>
2018-06-30 14:22:11 -0400

    Apple: "Let's break all our products for iPhone X."

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
+if(window.navigator.standalone) {
+  document.querySelector(`meta[name=viewport]`)
+    .setAttribute(`content`, `width=device-width, minimum-scale=1, initial-scale=1, shrink-to-fit=no`);
+}
+
 const root = document.getElementById(`root`);
 
 document.addEventListener(`touchmove`, (e)=> e.preventDefault(), { passive: false });
