af138a7f3f10f029e40ad83721d3925b8a7e2171
Brian Blakely <anewpage.media@gmail.com>
2018-07-13 19:24:28 -0400

    Clock mod UI: better spacing for devices without save zones.

--- src/components/QuickMenu/QuickMenuStyles.js
+++ src/components/QuickMenu/QuickMenuStyles.js
@@ -19,7 +19,7 @@ export const styleQuickMenu = withStyles({
   clock: {
     flexDirection: `row`,
     justifyContent: `space-around`,
-    padding: `env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)`,
+    padding: `6px max(6px, env(safe-area-inset-right)) max(6px, env(safe-area-inset-bottom)) max(6px, env(safe-area-inset-left))`,
 
     fallbacks: { padding: `6px` }
   },
