c55e06aa2eb0048bab92b9a309f987b0f44852c5
Brian Blakely <brian.blakely@aexp.com>
2018-07-18 13:58:55 -0400

    Create utils module.

--- src/components/Context.js
+++ src/components/Context.js
@@ -10,6 +10,8 @@ import JSZip from 'jszip';
 
 import { thumbs, games } from '../db/gameboy.js';
 
+import { getDataUri, buffersEqual } from '../utils';
+
 import {
   gameboy,
   settings,
@@ -26,44 +28,6 @@ import {
 } from '../cores/GameBoy-Online/index';
 
 const { Provider, Consumer } = createContext();
-
-const getDataUri = (url)=> new Promise((resolve)=> {
-  fetch(url).then((response)=> {
-    if(response.ok) {
-      return response.blob();
-    }
-    throw new Error(JSON.stringify(response));
-  }).then((blob)=> {
-    const reader = new FileReader();
-
-    reader.onloadend = ()=> {
-      resolve(reader.result);
-    };
-
-    reader.readAsDataURL(blob);
-  }).catch((error)=> {
-    console.warn(`Error getting data URI for ${url}:`, error);
-
-    resolve(false);
-  });
-});
-
-const buffersEqual = (buf1, buf2)=> {
-  if(buf1.byteLength !== buf2.byteLength) {
-    return false;
-  }
-
-  const ta1 = new Uint8Array(buf1),
-        ta2 = new Uint8Array(buf2);
-
-  for(const [i] of ta1.entries()) {
-    if(ta1[i] !== ta2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
 
 const getThumbUri = async (title)=> {
   const processUri = (uri)=> {
