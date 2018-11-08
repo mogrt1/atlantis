b3b74d26e16ad2d1894716868db64d17d3c50fb0
Brian Blakely <anewpage.media@gmail.com>
2018-11-08 13:30:20 -0500

    Port Sound to hooks.

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 import Notification from './Notification/Notification';
 
-import { Consumer } from './Context/Context';
+import { appContext } from './Context/Context';
 
-const Sound = ()=> (
-  <Consumer>
-    {({ state, actions })=> (
-      <Notification
-        open={state.audioNeedsConfirmation}
-        primaryAction={actions.enableAudio}
-        secondaryLabel="Enable"
-      >
-        {`Tap to enable sound.`}
-      </Notification>
-    )}
-  </Consumer>
-);
+const Sound = ()=> {
+  const { state, actions } = React.useContext(appContext);
+
+  return (
+    <Notification
+      open={state.audioNeedsConfirmation}
+      primaryAction={actions.enableAudio}
+      secondaryLabel="Enable"
+    >
+      {`Tap to enable sound.`}
+    </Notification>
+  );
+};
 
 export default Sound;
