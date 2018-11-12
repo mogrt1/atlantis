606d58ab9097d1cc343f22e931b1dfcddc4cebf0
Brian Blakely <anewpage.media@gmail.com>
2018-11-12 00:01:31 -0500

    Port Library to hooks.

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -12,62 +12,57 @@ import AddGame from "./AddGame";
 import GameList from "./GameList";
 import Homebrew from "./Homebrew";
 
-import { Consumer } from "../Context/Context";
+import { appContext } from "../Context/Context";
 
-class Library extends React.Component {
-  componentDidMount() {
+const Library = props => {
+  const { state, actions } = React.useContext(appContext);
+  const { classes } = props;
+
+  React.useEffect(() => {
     get(`games`).then(games => {
       if (!games) {
         return;
       }
 
-      this.props.addToLibrary(games);
+      props.addToLibrary(games);
     });
-  }
+  }, []);
 
-  render() {
-    const { classes } = this.props;
+  return (
+    <>
+      <IconButton
+        aria-label="open settings"
+        className={classes.open}
+        color="inherit"
+        onClick={actions.toggleDrawer(`library`)}
+      >
+        <LibraryIcon />
+      </IconButton>
 
-    return (
-      <Consumer>
-        {({ state, actions }) => (
-          <React.Fragment>
-            <IconButton
-              aria-label="open settings"
-              className={classes.open}
-              color="inherit"
-              onClick={actions.toggleDrawer(`library`)}
-            >
-              <LibraryIcon />
-            </IconButton>
+      <Drawer
+        anchor="right"
+        open={state.libraryOpen}
+        onClose={actions.toggleDrawer(`library`)}
+      >
+        <List
+          className={classes.drawer}
+          role="button"
+          subheader={
+            <ListSubheader className={classes.heading}>
+              {`Library`}
+            </ListSubheader>
+          }
+          tabIndex={0}
+        >
+          <AddGame />
+          <GameList />
+        </List>
 
-            <Drawer
-              anchor="right"
-              open={state.libraryOpen}
-              onClose={actions.toggleDrawer(`library`)}
-            >
-              <List
-                className={classes.drawer}
-                role="button"
-                subheader={
-                  <ListSubheader className={classes.heading}>
-                    {`Library`}
-                  </ListSubheader>
-                }
-                tabIndex={0}
-              >
-                <AddGame />
-                <GameList />
-              </List>
-
-              <Homebrew />
-            </Drawer>
-          </React.Fragment>
-        )}
-      </Consumer>
-    );
-  }
-}
+        <Homebrew />
+      </Drawer>
+    </>
+  );
+};
 
 Library.propTypes = {
   classes: PropTypes.objectOf(PropTypes.string).isRequired,
