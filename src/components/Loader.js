4ab5a12f51b8fdd31d2b3aac327e2dd4416fa95e
Brian Blakely <anewpage.media@gmail.com>
2018-11-12 00:15:09 -0500

    Port Loader to hooks.

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,56 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default class Loader extends React.Component {
-  constructor(props) {
-    super(props);
+const Loader = props => {
+  const { uri } = props;
 
-    this.load = () => {
-      const { uri } = this.props;
+  const load = () => {
+    fetch(uri)
+      .then(response => {
+        if (response.ok) {
+          return response.blob();
+        }
+        throw new Error(`Network response was not ok.`);
+      })
+      .then(blob => {
+        const reader = new FileReader();
 
-      fetch(uri)
-        .then(response => {
-          if (response.ok) {
-            return response.blob();
-          }
-          throw new Error(`Network response was not ok.`);
-        })
-        .then(blob => {
-          const reader = new FileReader();
+        reader.onloadend = () => {
+          props.setCurrentROM(reader.result);
+        };
 
-          reader.onloadend = () => {
-            this.props.setCurrentROM(reader.result);
-          };
+        reader.readAsArrayBuffer(blob);
+      })
+      .catch(error => {
+        console.error(
+          `There has been a problem with your fetch operation: `,
+          error.message
+        );
+      });
+  };
 
-          reader.readAsArrayBuffer(blob);
-        })
-        .catch(error => {
-          console.error(
-            `There has been a problem with your fetch operation: `,
-            error.message
-          );
-        });
-    };
-  }
+  React.useEffect(
+    () => {
+      load();
+    },
+    [uri]
+  );
 
-  componentDidMount() {
-    this.load();
-  }
-
-  shouldComponentUpdate(nextProps) {
-    return nextProps.uri && nextProps.uri !== this.props.uri;
-  }
-
-  componentDidUpdate() {
-    this.load();
-  }
-
-  render() {
-    return null;
-  }
-}
+  return null;
+};
 
 Loader.propTypes = {
   uri: PropTypes.string.isRequired,
   setCurrentROM: PropTypes.func.isRequired
 };
+
+export default Loader;
