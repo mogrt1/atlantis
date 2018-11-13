ce42b291ef8cb119ef7aa84abd9197f50b91aa36
Brian Blakely <anewpage.media@gmail.com>
2018-11-12 23:12:04 -0500

    Port TurboToggleButton to hooks.

--- src/components/TurboToggleButton.js
+++ src/components/TurboToggleButton.js
@@ -3,38 +3,30 @@ import PropTypes from "prop-types";
 
 import Button from "./Button/Button";
 
-export default class TurboToggleButton extends React.Component {
-  constructor(props) {
-    super(props);
+const TurboToggleButton = props => {
+  const pressed = React.useRef(false);
 
-    this.state = {};
-
-    let pressed = false;
-
-    this.events = {
-      down: () => {
-        if (pressed) {
-          return false;
-        }
-
-        pressed = true;
-
-        props.toggleTurbo();
-      },
-      up: () => {
-        pressed = false;
+  const events = {
+    down: () => {
+      if (pressed.current) {
+        return false;
       }
-    };
-  }
 
-  render() {
-    return (
-      <Button className={this.props.className} pointerCommands={this.events}>
-        {this.props.children}
-      </Button>
-    );
-  }
-}
+      pressed.current = true;
+
+      props.toggleTurbo();
+    },
+    up: () => {
+      pressed.current = false;
+    }
+  };
+
+  return (
+    <Button className={props.className} pointerCommands={events}>
+      {props.children}
+    </Button>
+  );
+};
 
 TurboToggleButton.propTypes = {
   toggleTurbo: PropTypes.func.isRequired,
@@ -46,3 +38,5 @@ TurboToggleButton.defaultProps = {
   className: ``,
   children: null
 };
+
+export default TurboToggleButton;
