32f0dd152d7a809866d968f1d20f1890534b8691
Brian Blakely <anewpage.media@gmail.com>
2018-11-12 22:14:54 -0500

    Port Settings to hooks.

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -16,79 +16,70 @@ import SettingsShowOverlay from "./SettingsShowOverlay";
 import SettingsKeyBindings from "./SettingsKeyBindings";
 import Link from "../Link/Link";
 
-import { Consumer } from "../Context/Context";
+import { appContext } from "../Context/Context";
 
-class Settings extends React.Component {
-  shouldComponentUpdate() {
-    return false;
-  }
+const Settings = props => {
+  const { state, actions } = React.useContext(appContext);
+  const { classes } = props;
 
-  render() {
-    const { classes } = this.props;
+  return (
+    <>
+      <IconButton
+        aria-label="open settings"
+        className={classes.open}
+        color="inherit"
+        onClick={actions.toggleDrawer(`settings`)}
+      >
+        <SettingsIcon />
+      </IconButton>
 
-    return (
-      <Consumer>
-        {({ state, actions }) => (
-          <React.Fragment>
-            <IconButton
-              aria-label="open settings"
-              className={classes.open}
-              color="inherit"
-              onClick={actions.toggleDrawer(`settings`)}
-            >
-              <SettingsIcon />
-            </IconButton>
-
-            <Drawer
-              onClose={actions.toggleDrawer(`settings`)}
-              open={state.settingsOpen}
-            >
-              <List
-                className={classes.drawer}
-                role="button"
-                subheader={
-                  <ListSubheader className={classes.heading}>
-                    {`Settings`}
-                  </ListSubheader>
-                }
-                tabIndex={0}
-              >
-                <SettingsMute />
-                {`vibrate` in window.navigator && <SettingsHaptics />}
-                <SettingsFFRate
-                  rate={state.settings.ffRate}
-                  updateSetting={actions.updateSetting(`ffRate`)}
-                />
-                <SettingsFFToggle />
-                <SettingsRewind />
-                <SettingsManageData
-                  deleteGame={actions.deleteGame}
-                  deleteSaveState={actions.deleteSaveState}
-                  deleteSRAM={actions.deleteSRAM}
-                  library={state.library}
-                />
-                <SettingsShowOverlay />
-                <SettingsKeyBindings
-                  keyBindings={state.settings.keyBindings}
-                  updateSetting={actions.updateSetting(`keyBindings`)}
-                />
-                <Link
-                  error
-                  href="https://github.com/brianblakely/atlantis/issues/new?template=bug-report.md"
-                >
-                  {`Report a Bug`}
-                </Link>
-              </List>
-            </Drawer>
-          </React.Fragment>
-        )}
-      </Consumer>
-    );
-  }
-}
+      <Drawer
+        onClose={actions.toggleDrawer(`settings`)}
+        open={state.settingsOpen}
+      >
+        <List
+          className={classes.drawer}
+          role="button"
+          subheader={
+            <ListSubheader className={classes.heading}>
+              {`Settings`}
+            </ListSubheader>
+          }
+          tabIndex={0}
+        >
+          <SettingsMute />
+          {`vibrate` in window.navigator && <SettingsHaptics />}
+          <SettingsFFRate
+            rate={state.settings.ffRate}
+            updateSetting={actions.updateSetting(`ffRate`)}
+          />
+          <SettingsFFToggle />
+          <SettingsRewind />
+          <SettingsManageData
+            deleteGame={actions.deleteGame}
+            deleteSaveState={actions.deleteSaveState}
+            deleteSRAM={actions.deleteSRAM}
+            library={state.library}
+          />
+          <SettingsShowOverlay />
+          <SettingsKeyBindings
+            keyBindings={state.settings.keyBindings}
+            updateSetting={actions.updateSetting(`keyBindings`)}
+          />
+          <Link
+            error
+            href="https://github.com/brianblakely/atlantis/issues/new?template=bug-report.md"
+          >
+            {`Report a Bug`}
+          </Link>
+        </List>
+      </Drawer>
+    </>
+  );
+};
 
 Settings.propTypes = {
   classes: PropTypes.objectOf(PropTypes.string).isRequired
 };
 
-export default styleSettings(Settings);
+export default React.memo(styleSettings(Settings), () => true);
