2f2525caa9b2d8376f7f8e30395cf01cd1d55b29
Brian Blakely <anewpage.media@gmail.com>
2019-02-19 00:54:45 -0500

    changes for testing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-// Basic global setup. The app proper begins in App.
-
 import React from "react";
 import ReactDOM from "react-dom";
 
-import initialState from "./initialState";
-import Context from "./components/Context/Context";
-import App from "./components/App/App";
+import { unstable_Box as Box } from "@material-ui/core/Box";
 
-import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(
-  <Context initialState={initialState}>
-    <App />
-  </Context>,
-  document.getElementById(`root`)
-);
-registerServiceWorker();
+ReactDOM.render(<Box>Testing</Box>, document.getElementById(`root`));
