4b651d55bb232e0937bfbcfbaaa77c6dfe8fe303
Brian Blakely <anewpage.media@gmail.com>
2020-01-12 01:57:48 -0500

    First unit test passed!

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import { render } from "@testing-library/react";
+
+import { Providers } from "../../setupTests";
 
 import App from "./App";
 
 it(`renders without crashing`, () => {
-  const div = document.createElement(`div`);
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  act(() => {
+    render(<App />, { wrapper: Providers });
+  });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "fake-indexeddb/auto";
 // react-testing-library renders your components to document.body,
 // this adds jest-dom's custom assertions
 import "@testing-library/jest-dom/extend-expect";
+
+import Context from "./components/Context/Context";
+import initialState from "./initialState";
+
+export const Providers = ({ children }) => (
+  <div id="root">
+    <Context initialState={initialState}>{children}</Context>
+  </div>
+);
+
+Providers.propTypes = {
+  children: PropTypes.node
+};
