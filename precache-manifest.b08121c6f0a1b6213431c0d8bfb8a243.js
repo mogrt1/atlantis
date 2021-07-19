015dbff36fef49f3fadac1373b683e063c3d2d2c
Brian Blakely <anewpage.media@gmail.com>
2021-07-18 23:28:31 -0400

    Updates

---
+++ precache-manifest.b08121c6f0a1b6213431c0d8bfb8a243.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e35f89184afa344917ce576fa2452d",
+    "url": "/atlantis/index.html"
+  },
+  {
+    "revision": "d6d5389dd991b9fe9724",
+    "url": "/atlantis/static/js/2.3f98192a.chunk.js"
+  },
+  {
+    "revision": "5f5ca9dccbfa3236d2de99c480d68e94",
+    "url": "/atlantis/static/js/2.3f98192a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e454b1d8fa98160ac9de",
+    "url": "/atlantis/static/js/main.cd809fdd.chunk.js"
+  },
+  {
+    "revision": "16cf8bd06ab2bd78355d",
+    "url": "/atlantis/static/js/runtime-main.14917799.js"
+  },
+  {
+    "revision": "0c981b1ea4ee3d5012b38e76385058fe",
+    "url": "/atlantis/static/media/flappyboy.gb.0c981b1e.zip"
+  },
+  {
+    "revision": "2ca94b9cb3f02e7bb5836c19a8b5f3c2",
+    "url": "/atlantis/static/media/gameboy.2ca94b9c.svg"
+  },
+  {
+    "revision": "830a393c7fa03a4960697bb694c90cf6",
+    "url": "/atlantis/static/media/infinity.gb.830a393c.zip"
+  },
+  {
+    "revision": "605b7db7b8a7f0aac7a8dbeb41bb36e4",
+    "url": "/atlantis/static/media/postbot.gb.605b7db7.zip"
+  },
+  {
+    "revision": "4b4c65205ff60c5fa1f88af533ff0d1d",
+    "url": "/atlantis/static/media/space-invasion.gb.4b4c6520.zip"
+  }
+]);
